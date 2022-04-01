1401b1c1486c829cc0d5199a9835cff3b64a3fd7
00wgHello <854191570@qq.com>
2022-04-01 17:47:40 +0800

    第二次

--- src/views/code/PyThon.vue
+++ src/views/code/PyThon.vue
@@ -3,11 +3,28 @@
     <h3>我是python工程师</h3> 
     <img src="../../assets/logo.png" alt="">   
   </div>
+  <br>
+  <input type="text" v-model="txt"><br>
+  <button @click="newtj">点击页面跳转</button>
+  
 </template>
 
 <script>
+import {useRouter} from 'vue-router'
+import {ref} from 'vue'
 export default {
   name:'PyThon',
+  setup(){
+    let txt = ref('')
+    const router = useRouter()
+    const newtj = () => {
+      // 第一种格式发送
+      router.push({path:'/TypeScripy',query:{user:txt.value}})
+      //第二种格式发送
+      // router.push({name:'TypeScripy',params:{name:txt.value}})
+    }
+    return {txt,newtj}
+  }
 }
 </script>
 
--- src/views/code/TypeScripy.vue
+++ src/views/code/TypeScripy.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
     <h3>我是TypeScripy工程师</h3>
-    <img :src="urlimg" alt="">
+    <img :src="urlimg" alt=""><br>
+    <h1>{{txt}}</h1>
   </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
+import { ref } from 'vue'
+import {useRoute} from 'vue-router'
 export default {
     name:'TypeScripy',
     setup(){
       let urlimg = ref(require('../../assets/logo.png'))
-      return {urlimg}
+      
+      const msg = ref('')
+      const route = useRoute()
+      msg.value = route.query.user
+      //第一种格式接收:
+      // console.log(route.query.user);
+      //第二种格式
+      // console.log(route.params.name);
+      return {urlimg,route,msg}
+    },
+    computed:{
+      txt(){
+        return this.route.query.user;
+      }
     }
 }
 </script>
